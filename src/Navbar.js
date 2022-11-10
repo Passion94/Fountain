4b44bd3a1782af4a83bc44cbc6ee0c333c9524b0
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-10 10:12:07 -0800

    Third commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
 import './index.css';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
 function Navbar(){
 return (<div>
 <div className="navbar">
     <div >
-    <i class='fas fa-bars'> <span id='fountain'>Fountain</span></i>
+     <span><MenuOutlinedIcon className='fas fa-bars' fontSize='30px'/><span id='fountain'>Fountain</span></span>
     
 </div>
  <AccountCircleOutlinedIcon className='far fa-user-circle' fontSize='35px'/>
