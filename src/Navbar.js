91b779522ff4073dd853160d41d58e91af82c545
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-11 00:51:20 -0800

    Complete header banner mobile and laptop

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,27 @@
 import './index.css';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 function Navbar(){
 return (<div>
 <div className="navbar">
     <div >
      <span id='bars-container'><MenuOutlinedIcon className='fas fa-bars' fontSize='30px'/><span id='fountain'>Fountain</span></span>
-</div><div>
+</div>
+<div className='hidden-Navbar-links'>
+<a href="#" className='Navbar-links'> Home</a>
+<a href="#" className='Navbar-links'> Courses</a>
+<a href="#" className='Navbar-links'> Instructors</a>
+<a href="#" className='Navbar-links'> Schedules</a>
+<a href="#" className='Navbar-links'>Contact us</a>
+<div className='searchbar'>
+<a href="#" className='Navbar-links1' id='search'><SearchOutlinedIcon/></a>
+<a href="#" className='Navbar-links1' id='login'>Login</a> 
+<a href="#" className='Navbar-links1' id='register'>Register</a> 
+</div>
+</div>
+<div>
  <AccountCircleOutlinedIcon className='far fa-user-circle' fontSize='35px'/>
      </div> 
      </div>
